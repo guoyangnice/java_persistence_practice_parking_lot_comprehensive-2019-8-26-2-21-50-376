2880fdc360b778833be19bd890c934a2b2269d8d
guoyangnice <yangjienice@126.com>
2019-08-30 23:13:52 +0800

    添加parkingboy实体类

---
+++ src/main/java/tws/entity/Parkingboy.java
@@ -0,0 +1,15 @@
+package tws.entity;
+
+public class Parkingboy {
+	private String employeeID;
+
+	public String getEmployeeID() {
+		return employeeID;
+	}
+
+	public void setEmployeeID(String employeeID) {
+		this.employeeID = employeeID;
+	}
+	
+	
+}
